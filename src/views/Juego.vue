0f10a8fb41ab89f26afc9b7686b4dd412405b798
Duban Ruales <duvanruales99@gmail.com>
2020-12-07 18:49:23 -0500

    Controlando alertas y vistas - funcional

--- src/views/Juego.vue
+++ src/views/Juego.vue
@@ -414,7 +414,7 @@ export default {
       );
     },
 
-    //juego en pausas
+    //juego en pausa intermediarios 
     pause() {
       clearInterval(cronometro);
       swal({
@@ -422,14 +422,14 @@ export default {
         icon:
           "https://pauseonline.s3.eu-west-2.amazonaws.com/wp-content/uploads/2019/11/01131317/Pause-Logo.png",
 
-        text: "ACIERTOS: " + this.aciertos,
+        text: "ACIERTOS: "+"[ " + this.aciertos + " ]",
 
         buttons: {
           reanudar: {
             text: "REANUDAR",
           },
           home: {
-            text: "SALIR",
+            text: "INICIO ",
           },
         },
         closeOnClickOutside: false,
@@ -451,14 +451,14 @@ export default {
       clearInterval(cronometro);
       swal({
         icon: "https://www.flaticon.es/svg/static/icons/svg/1986/1986987.svg",
-        title: "Intentos: " + this.intentos,
-        text: "Tiempo  " + this.min + ":" + this.seg,
+        title: "INTENTOS FALLIDOS: "+"[ " + this.intentos +" ]",
+        text: "TIEMPO:  " + this.min + ":" + this.seg,
         buttons: {
           reiniciar: {
-            text: "Reiniciar",
+            text: "REINICIAR",
           },
           home: {
-            text: "Salir",
+            text: "INICIO ",
           },
         },
         closeOnClickOutside: false,
